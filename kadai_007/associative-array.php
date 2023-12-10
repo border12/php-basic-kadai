f97223c1595e32244ae1fca78712e95ad9c520f0
border12 <139112044+border12@users.noreply.github.com>
2023-12-11 08:33:20 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+<body>
+<p>
+  <?php
+  // 連想配列を宣言・代入
+  $vegetable = [ 'name' => 'onion', 'price' => 200, 'weight' => 160 ]; 
+  print_r($vegetable);
+?>
+</p>
+</body>
+</html>
